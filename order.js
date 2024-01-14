99550c24409a5ec3b5ab633610c100bb833e31b7
Xlink <3095421243@qq.com>
2024-01-14 23:25:23 +0800

    dashboard

---
+++ order.js
@@ -0,0 +1,26 @@
+const Orders = [
+    {
+        productName: '例子数据',
+        productNumber: '85743',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'JavaScript Tutorial',
+        productNumber: '85743',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'CSS Full Course',
+        productNumber: '97245',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Flex-Box Tutorial',
+        productNumber: '36452',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
